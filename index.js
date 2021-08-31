4a9cb61d3a70a55f6a0c34868d033654ccdd115b
hungpk <hungpk1412@gmail.com>
2021-08-31 21:30:54 +0700

    Query parameters

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+var users= [
+  {id: 1, name: 'Thing'},
+  {id: 2, name: 'Tho'},
+  {id: 3, name: 'Toan'},
+  {id: 4, name: 'Hung'},
+];
+
+app.use(express.static('public'))
+
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+app.use('/static', express.static('public'))
+// http://localhost:3000/static/sea.jpg
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Aloha', message: 'Hello ' });
@@ -11,12 +23,19 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
   res.render('users/index', {
-    users: [
-      {id: 1, name: 'Thing'},
-      {id: 2, name: 'Tho'},
-      {id: 3, name: 'Toan'},
-      {id: 4, name: 'Hung'},
-    ]
+    users: users
+  });
+});
+
+app.get('/users/search', (req, res) => {
+  var q = req.query.q.toLowerCase();
+  var matchedUsers = users.filter((user) => {
+    return user.name.toLowerCase().indexOf(q) !== -1;
+  }); 
+  
+  res.render('users/index', {
+    users: matchedUsers,
+    text: q
   });
 });
 
