b2c2fd0892b034d5f336c7498b6faba3eedeb6eb
hungpk <hungpk1412@gmail.com>
2021-08-31 20:45:09 +0700

    Template engines

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-    res.send('<h2>Hello</h2>');
+  res.render('index', { title: 'Aloha', message: 'Hello ' });
+});
+
+app.get('/users', (req, res) => {
+  res.render('users/index', {
+    users: [
+      {id: 1, name: 'Thing'},
+      {id: 2, name: 'Tho'},
+      {id: 3, name: 'Toan'},
+      {id: 4, name: 'Hung'},
+    ]
+  });
 });
 
 app.listen(3000, (req, res) => {
-    console.log('Server start on port 3000');
+  console.log('Server start on port 3000');
 });
