4bf8ae7d730d84730bdd89cbf6afb22b002de5ab
hungpk <hungpk1412@gmail.com>
2021-09-06 15:42:59 +0700

    API demo

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -6,10 +6,62 @@ module.exports.index = function(req, res) {
   res.json(products);
 };
 
+module.exports.getProduct = function(req, res, next) {
+
+  var product = db.get('products').find({ id: req.params.id }).value();
+  res.json(product);
+}
+
 module.exports.create = async function(req, res, next) {
   var product = req.body;
   await db.get('products').push(product).write();
   res.json(product);
+};
+
+module.exports.putProduct = async function(req, res, next) {
+  var product = req.body;
+  product.id = req.params.id;
+  
+  var resultProduct = db.get('products').find({ id: req.params.id }).value();
+
+  if(!resultProduct) {
+    await db.get('products').push(product).write();
+  } else {
+    await db.get('products').find({ id: req.params.id}).assign({ 
+      name: product.name,
+      description: product.description,
+      price: product.price
+    }).write();
+  }
+
+  res.json(product);
+}
+
+module.exports.patchProduct = async function(req, res, next) {
+  var resultProduct = db.get('products').find({ id: req.params.id }).value();
+  var product = req.body;
+  if(!resultProduct) {
+    res.json();
+    return;
+  }
+
+  await db.get('products').find({ id: req.params.id}).assign({ 
+    name: product.name,
+    description: product.description,
+    price: product.price
+  }).write();
+
+  resultProduct = db.get('products').find({ id: req.params.id }).value();
+  res.json(resultProduct);
+  
+};
+
+module.exports.deleteProduct = function(req, res, next) {
+  var resultProduct = db.get('products').find({ id: req.params.id }).value();
+  db.get('products').remove({ id: req.params.id })
+  .write();
+
+  res.json(resultProduct);
 };
 
 // TODO Full REST API
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -6,6 +6,14 @@ const router = express.Router();
 
 router.get('/', controller.index);
 
+router.get('/:id', controller.getProduct)
+
 router.post('/', controller.create);
+
+router.put('/:id', controller.putProduct);
+
+router.patch('/:id', controller.patchProduct);
+
+router.delete('/:id', controller.deleteProduct);
 
 module.exports = router;
